fd224d9de61224d3c62effc85839290ddb6dfbed
Кирилл Гулиев <guliev@ecopsy.ru>
2021-08-19 14:35:50 +0300

    final commit and python script

--- R/parse.R
+++ R/parse.R
@@ -32,9 +32,9 @@ link1_final <- cbind(dates_link1, comments_link1)
 
 #rsDriver()
 
-rs <- rsDriver(browser = "firefox", check = FALSE)
-
-rd <- rsDriver(browser="chrome",
-               extraCapabilities=list("C://Users//Кирилл//Desktop//RRR//driver//chromedriver.exe"),
-               chromever="92.0.4515.107",
-               check = FALSE)
+# rs <- rsDriver(browser = "firefox", check = FALSE)
+# 
+# rd <- rsDriver(browser="chrome",
+#                extraCapabilities=list("C://Users//Кирилл//Desktop//RRR//driver//chromedriver.exe"),
+#                chromever="92.0.4515.107",
+#                check = FALSE)
